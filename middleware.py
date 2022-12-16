df814ab56a40b2a3b4fb8e49ab372d616515040a
Vlad <vlad7772004@gmail.com>
2022-12-16 21:37:59 +0300

    create_db.py to creadte database

--- middleware.py
+++ middleware.py
@@ -11,13 +11,5 @@ class LoggingMiddleware(BaseMiddleware):
         super(LoggingMiddleware, self).__init__()
 
     async def on_process_message(self, message: types.Message, data: dict) -> None:
-        # print(args, kwargs)
         logging.info(f"User ({message.from_user.id}, @{message.from_user.username}, "
                      f"{message.from_user.first_name}) sent: {message.text}")
-        # print("Hello, world!")
-
-# class LoggingMiddleware(BaseMiddleware):
-#     """Logs user messages as info."""
-#     async def __call__(self, handler, msg: Message, data):
-#         logging.info(f"User ({msg.from_user.id}, {msg.from_user.username}, {msg.from_user.first_name}) "
-#                      f"sent: {msg.text}")

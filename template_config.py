a15094e586cf3986703742425798d4cb73697c98
Vlad <vlad7772004@gmail.com>
2023-04-21 22:48:18 +0300

    template configure file

--- template_config.py
+++ template_config.py
@@ -1,4 +1,4 @@
-TELEGRAM_TOKEN = "YOUR_OWN_TELEGRAM_TOKEN"
+TELEGRAM_TOKEN = "YOUR_OWN_TELEGRdfsfdAM_TOKEN"
 CREATOR = "@your_short_name"
 BOT_EMAIL = "BOT_EMAIL_ADDRESS"
 LINK_TO_GOOGLE_SHEET = "https://docs.google.com/spreadsheets/d/1mlQx8YjeBeJNxNdgzQGsj14KFdC8Drz6dJJP8aKfY9Y/edit?usp=sharing"
